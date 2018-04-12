94cdbf8cea094eeb32e45faacacc43761dfad1e4
Jon Sangster <jon@ertt.ca>
2018-04-12 01:52:25 -0700

    Add LICENSE

--- font.h
+++ font.h
@@ -1,5 +1,23 @@
 #ifndef IMG2PCD8544_FONT_H
 #define IMG2PCD8544_FONT_H
+/* @file
+ *
+ * Arduino Pong is a Pong clone written for the Arduino Uno (or similar).
+ * Copyright (C) 2018  Jon Sangster
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <https://www.gnu.org/licenses/>.
+ */
 
 #include <avr/pgmspace.h>
 #include <sangster/pcd8544/text.h>
